a92a2837aa66a1e804d432e146d12d8b8eaeabc8
Don Nguyen <thedonwind69@gmail.com>
2020-08-30 13:11:57 -0500

    set up seeds

---
+++ frontend/components/createSaleForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class createSaleForm extends Component {
+    render() {
+        return (
+            <div>
+                sale form here
+            </div>
+        )
+    }
+}
+
+export default createSaleForm
--- frontend/components/inventory.jsx
+++ frontend/components/inventory.jsx
@@ -1,12 +1,17 @@
 import React, { Component, useState, useReducer} from 'react'
+import CreateSaleForm  from './createSaleForm';
 
 export default function Inventory () {
-
-
 
     return (
         <div>
             currentUser.lightsabers
+
+            put all the ones for sale first "
+            
+                currentUser.lighsabers.filter((lightsaber)
+
+            <CreateSaleForm />
         </div>
     )
 }
