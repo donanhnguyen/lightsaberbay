11c4d5e4adef13e8ae50c64ec602530a5c8f7d5f
Don Nguyen <thedonwind69@gmail.com>
2020-10-28 13:55:12 -0500

    shopping cart still

--- frontend/components/userInfo.jsx
+++ frontend/components/userInfo.jsx
@@ -1,9 +1,18 @@
 import React, {useReducer, useEffect} from 'react'
 
 export default function UserInfo({userState}) {
+
+    var localCart = JSON.parse(localStorage.getItem('Cart'));
+    console.log(localCart);
+
+    function numberOfItemsInYourCart () {
+        if (localCart) {return <h1 class='new-messages'>{localCart.length} items in your shopping cart</h1>}
+    }
+
     if (userState) {
         return (
             <div class="userInfoBar">
+                {numberOfItemsInYourCart()}
                 Your Credits: {userState.credits}
              </div>
         )
