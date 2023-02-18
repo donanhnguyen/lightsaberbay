ac485ccc0f84c5ff0dab07539ab6590b47573242
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-02-17 23:04:32 -0600

    last try for tonight, adding webpack commands to render-build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,8 +7,8 @@ bundle install
 rm -rf public
 # build
 npm install
+npm run webpack:deploy
 # migrate
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-npm run webpack
